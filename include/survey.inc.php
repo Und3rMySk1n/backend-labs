7798d4b4ac8553fce588e836e67546d75965ed39
Und3rMySk1n <undermyskin@inbox.ru>
2015-05-25 21:50:26 +0300

    1. Заданы константы и вывод ошибок error_reporting(E_ALL) для проверки.

--- include/survey.inc.php
+++ include/survey.inc.php
@@ -1,5 +1,19 @@
 <?php
-
+    define('ERR_OK', 0);
+    define('ERR_NO_SURVEY', 1);
+    define('ERR_NO_FIRSTNAME', 2);
+    define('ERR_NO_LASTNAME', 3);
+    define('ERR_NO_EMAIL', 4);
+    define('ERR_NO_AGE', 5);
+    define('ERR_NO_ACCESS_TO_FILE', 6);
+    define('ERR_UNABLE_TO_PARSE', 7);
+    define('ERR_NO_FILE', 8);
+    define('ERR_FILE_NOT_WRITTEN', 9);
+    define('first_name', 10);
+    define('last_name', 11);
+    define('age', 12);
+    define('email', 13);
+    
     function PrintError($errorCode)
     {
         $errMessages = array //расшифровка ошибок записи
@@ -11,9 +25,7 @@
             ERR_NO_AGE => 'Не задан возраст. <br>',
             ERR_NO_ACCESS_TO_FILE => 'Нет доступа к файлу. <br>',
             ERR_UNABLE_TO_PARSE => 'Неправильный формат файла. <br>',
-            ERR_NO_FILE => 'Такого файла не существует. <br>',
-            ERR_NO_ACCESS_TO_FILE => 'Нет доступа к файлу. <br>',
-            ERR_UNABLE_TO_PARSE => 'Неправильный формат файла. <br>',
+            ERR_NO_FILE => 'Такого файла не существует. <br>',           
             ERR_FILE_NOT_WRITTEN => 'Не удалось записать файл. <br>'
         );
 
@@ -80,7 +92,7 @@
 
     function SaveSurveyToFile($userInfo, &$errorCode)
     {
-        $surveyFilePath = GetSurveyFilePath($userInfo['email']);
+        $surveyFilePath = GetSurveyFilePath($userInfo[email]);
         $newfile = fopen($surveyFilePath, 'w');
         $wrt = fwrite($newfile, serialize($userInfo));
         if ($wrt == false)
@@ -134,8 +146,8 @@
 
     function PrintSurvey($survey)
     {
-        echo '<b>First Name:</b> ' . $survey['first_name'] . '<br>';
-        echo '<b>Last Name:</b> ' . $survey['last_name'] . '<br>';
-        echo '<b>Email:</b> ' . $survey['email'] . '<br>';
-        echo '<b>Age:</b> ' . $survey['age'] . '<br>';
+        echo '<b>First Name:</b> ' . $survey[first_name] . '<br>';
+        echo '<b>Last Name:</b> ' . $survey[last_name] . '<br>';
+        echo '<b>Email:</b> ' . $survey[email] . '<br>';
+        echo '<b>Age:</b> ' . $survey[age] . '<br>';
     }
--- surveyinfo.php
+++ surveyinfo.php
@@ -1,6 +1,7 @@
 <?php
     require_once('include/common.inc.php');
-
+    error_reporting(E_ALL);
+    
     $errorCode = ERR_OK;
     $surveyName = GetSurveyName($errorCode);
     if ($surveyName != false)
